f7ba326db0a8686099f3413d8303cc1fb77b618a
fengjutian <fengjutian@wxadt.com>
2020-04-13 22:23:08 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+    var msgArr = null
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+
+    socket.on('sendMsg', function (data) {
+        socket.emit('acceptMsg', data)  
+    });
+        
+  
+});
+
